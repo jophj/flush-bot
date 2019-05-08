b05fff12143d58cc5bffbd7fb54a4bdfd2440f14
Giovanni Pinto <jop.mezzana@gmail.com>
2019-05-08 19:20:31 +0200

    Initial release

---
+++ flush-bot.js
@@ -0,0 +1,12 @@
+setTimeout(() => process.exit(0), 10000)
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf(process.argv[2])
+bot.on('callback_query', (ctx) => {
+	console.log(ctx.callbackQuery)
+})
+bot.on('message', (ctx) => {
+	console.log(ctx.message)
+})
+bot.launch()
+
